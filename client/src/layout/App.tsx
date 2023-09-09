ee0c21be01710346ddb108f1ac7d6e69f732d7b6
Luke Reed <luke.reed@outlook.com.au>
2023-09-09 16:45:46 +1000

    Cataloge component

--- client/src/layout/App.tsx
+++ client/src/layout/App.tsx
@@ -1,13 +1,20 @@
 import Catalogue from "../features/catalogue/Catalogue";
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Header from "./Header";
+import { useState } from "react";
 
 function App() {
+	const [darkMode, setDarkMode] = useState(false);
+	const paletteType = darkMode ? "dark" : "light";
 	const theme = createTheme({
 		palette: {
-			mode: "dark",
+			mode: paletteType,
 		},
 	});
+
+	function toggleSwitch() {
+		setDarkMode(prevState);
+	}
 
 	return (
 		<>
