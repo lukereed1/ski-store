8a7e273d38c61e7839b03fdbcac533c567756c6d
Luke Reed <luke.reed@outlook.com.au>
2023-10-03 13:01:14 +1100

    Created order endpoijnts

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -4,7 +4,6 @@ using API.Entities;
 using API.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualBasic;
 
 namespace API.Controllers
 {
---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,110 @@
+using API.Data;
+using API.DTOs;
+using API.Entities.OrderAggregate;
+using API.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class OrdersController : BaseApiController
+    {
+        private readonly StoreContext _context;
+        public OrdersController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Order>>> GetOrders()
+        {
+            return await _context.Orders
+                .Include(o => o.OrderItems)
+                .Where(b => b.BuyerId == User.Identity.Name)
+                .ToListAsync();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            return await _context.Orders
+                .Include(o => o.OrderItems)
+                .Where(b => b.BuyerId == User.Identity.Name && b.Id == id)
+                .FirstOrDefaultAsync();
+        }
+
+        [HttpPost("{id}", Name = "GetOrder")]
+        public async Task<ActionResult<int>> CreateOrder(CreateOrderDto orderDto)
+        {
+            var basket = await _context.Baskets
+                .RetrieveBasketWithItems(User.Identity.Name)
+                .FirstOrDefaultAsync();
+
+            if (basket == null) return BadRequest(new ProblemDetails { Title = "Could not locate basket" });
+
+            var items = new List<OrderItem>();
+
+            foreach (var item in basket.Items)
+            {
+                var productItem = await _context.Products.FindAsync(item.ProductId);
+                var itemOrdered = new ProductItemOrdered
+                {
+                    ProductId = productItem.Id,
+                    Name = productItem.Name,
+                    PictureUrl = productItem.PictureUrl,
+
+                };
+
+                var orderItem = new OrderItem
+                {
+                    ItemOrdered = itemOrdered,
+                    Price = productItem.Price,
+                    Quantity = item.Quantity
+                };
+
+                items.Add(orderItem);
+                productItem.QuantityInStock -= item.Quantity;
+            }
+
+            var subtotal = items.Sum(item => item.Price * item.Quantity);
+            var deliveryFee = subtotal > 10000 ? 0 : 500;
+
+            var order = new Order
+            {
+                OrderItems = items,
+                BuyerId = User.Identity.Name,
+                ShippingAddress = orderDto.ShippingAddress,
+                Subtotal = subtotal,
+                Delivery = deliveryFee
+            };
+
+            _context.Orders.Add(order);
+            _context.Baskets.Remove(basket);
+
+            if (orderDto.SaveAddress)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity.Name);
+                user.Address = new UserAddress
+                {
+                    FullName = orderDto.ShippingAddress.FullName,
+                    Address1 = orderDto.ShippingAddress.Address1,
+                    Address2 = orderDto.ShippingAddress.Address2,
+                    City = orderDto.ShippingAddress.City,
+                    State = orderDto.ShippingAddress.State,
+                    Zip = orderDto.ShippingAddress.Zip,
+                    Country = orderDto.ShippingAddress.Country,
+                };
+                _context.Update(user);
+            }
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return CreatedAtRoute("GetOrder", new { id = order.Id }, order.Id);
+
+            return BadRequest("Problem creating order");
+        }
+    }
+}
